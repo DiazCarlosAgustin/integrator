00fdd4eef08a4dffb54e823b303286e3335d8de4
Carlos Diaz <carlos.agustin.diaz1998@gmail.com>
2023-06-22 14:35:25 -0300

    new routes on parameters

--- apps/parameters/src/parameters.controller.ts
+++ apps/parameters/src/parameters.controller.ts
@@ -22,24 +22,59 @@ export class ParametersController {
       .then((response) => {
         this.rmqService.ack(context);
         return response;
+      })
+      .catch((err) => {
+        return { error: true, message: err.message };
       });
   }
 
   @MessagePattern({ cmd: 'get_parameters' })
   async getAllParameters(@Ctx() context: RmqContext) {
-    return await this.parametersService.getAllParameters().then((response) => {
-      this.rmqService.ack(context);
-      return response;
-    });
+    return await this.parametersService
+      .getAllParameters()
+      .then((response) => {
+        this.rmqService.ack(context);
+        return response;
+      })
+      .catch((err) => {
+        return { error: true, message: err.message };
+      });
   }
 
-  @MessagePattern({ cmd: 'get_one__parameters' })
+  @MessagePattern({ cmd: 'get_one_parameters' })
   async getOneParameter(@Payload() data, @Ctx() context: RmqContext) {
     return await this.parametersService
       .getOneParameter(data)
       .then((response) => {
         this.rmqService.ack(context);
         return response;
+      })
+      .catch((err) => {
+        return { error: true, message: err.message };
+      });
+  }
+  @MessagePattern({ cmd: 'delete_parameters' })
+  async deleteParameter(@Payload() data, @Ctx() context: RmqContext) {
+    return await this.parametersService
+      .deleteParameter(data)
+      .then((response) => {
+        this.rmqService.ack(context);
+        return response;
+      })
+      .catch((err) => {
+        return { error: true, message: err.message };
+      });
+  }
+  @MessagePattern({ cmd: 'update_parameters' })
+  async updateParameter(@Payload() data, @Ctx() context: RmqContext) {
+    return await this.parametersService
+      .updateParameter(data)
+      .then((response) => {
+        this.rmqService.ack(context);
+        return response;
+      })
+      .catch((err) => {
+        return { error: true, message: err.message };
       });
   }
 }
